491c8d758b3515066247271e48e3c1a144a2e678
Dayo Farayola <97878920+s8nclone@users.noreply.github.com>
2023-06-08 06:01:41 +0100

    initialized repo and added files

---
+++ assets/admin.js
@@ -0,0 +1,21 @@
+// add hovered class to selected list item
+let navList = document.querySelectorAll(".navigation li");
+
+function activeNavLink() {
+  navList.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
+
+navList.forEach((item) => item.addEventListener("mouseover", activeNavLink));
+
+// Menu Toggle
+let toggle = document.querySelector(".hamburger_menu");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
